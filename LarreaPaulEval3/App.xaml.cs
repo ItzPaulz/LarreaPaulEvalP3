03eac9a59912434ba7f72d34f71f3d3d9e656e47
laboratorio <laboratorio@UPOAULA10323>
2025-01-21 07:23:50 -0500

    Agregar archivos de proyecto.

---
+++ LarreaPaulEval3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LarreaPaulEval3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
