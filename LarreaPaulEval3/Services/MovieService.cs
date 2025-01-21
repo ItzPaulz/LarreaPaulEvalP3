75c411a36cb9996a31110acfc079c330f8af5f2e
ItzPaulz <paullarrea2004@gmail.com>
2025-01-21 17:41:39 -0500

    Quite los 2 servicios para hacerle solo 1

---
+++ LarreaPaulEval3/Services/MovieService.cs
@@ -0,0 +1,35 @@
+﻿using SQLite;
+using LarreaPaulEval3.Models;
+using System.IO;
+
+namespace LarreaPaulEval3.Services
+{
+    public class MovieService
+    {
+        private readonly SQLiteConnection _database;
+
+        public MovieService()
+        {
+            
+            var dbPath = Path.Combine(FileSystem.AppDataDirectory, "plarrea_movies.db");
+            _database = new SQLiteConnection(dbPath);
+            _database.CreateTable<Pelicula>();
+        }
+
+
+        public void AddMovie(Pelicula pelicula)
+        {
+            _database.Insert(pelicula);
+        }
+
+        public List<Pelicula> GetMovies()
+        {
+            return _database.Table<Pelicula>().ToList();
+        }
+
+        public void DeleteAllMovies()
+        {
+            _database.DeleteAll<Pelicula>();
+        }
+    }
+}
